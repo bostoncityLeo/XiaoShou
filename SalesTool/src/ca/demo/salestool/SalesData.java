a01fa3736ee01c566c98af4eed470f4d21a75036
bostoncityLeo <fenway.leo@gmail.com>
2015-06-27 18:02:05 -0400

    first commit by Leo

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -11,7 +11,7 @@ public class SalesData {
 
 		int sum = 0;
 		for (int i: data) {
-			System.out.println("Next value: " + data[i]);
+			System.out.println("Next value: " + i);
 			sum += i;
 		}
 		
